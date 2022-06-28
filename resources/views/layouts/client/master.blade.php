da649b1e94fb1a48fb426b0b9c48dd433cc5ed96
nsenja <nabilasw34@gmail.com>
2022-06-28 11:03:41 +0700

    Creating master

---
+++ resources/views/layouts/client/master.blade.php
@@ -0,0 +1,7 @@
+    <!-- Header Section Begin -->
+    @include('layouts.client.partials.header')
+    <!-- Header Section End -->
+
+    <!-- Footer Section Begin -->
+    @include('layouts.client.partials.footer')
+    <!-- Footer Section End -->
