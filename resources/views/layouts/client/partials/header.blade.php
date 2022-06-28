3c66071475fb30ed01db9879c44b418a076771a3
Redhiafr <redhiafrr@gmail.com>
2022-06-28 10:38:49 +0700

    Layouting Header Footer

---
+++ resources/views/layouts/client/partials/header.blade.php
@@ -0,0 +1,130 @@
+@section('header')
+<header class="main-header header-style-two">
+    <!-- Header Top Two -->
+    <div class="header-top-two">
+        <div class="auto-container">
+            <div class="inner">
+                <div class="top-left">
+                    <ul class="contact-info">
+                        <li><a href="mailto:info@webmail.com"><i class="far fa-envelope"></i>info@webmail.com</a></li>
+                        <li><a href="tel:+98787687676"><i class="far fa-phone"></i>+987 876 876 76</a></li>
+                    </ul>
+                </div>
+
+                <div class="top-middile">
+                    <div class="text">Welcome To Our Company</div>
+                </div>
+
+                <div class="top-right">
+                    <div class="language">
+                        <form action="#" class="language-switcher">
+                            <select class="selectpicker">
+                                <option value="1">EN</option>
+                                <option value="1">FR</option>
+                                <option value="1">SP</option>
+                                <option value="1">BE</option>
+                            </select>
+                        </form>
+                    </div>
+                    <ul class="social-links clearfix">
+                        <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fab fa-behance"></span></a></li>
+                        <li><a href="#"><span class="fab fa-linkedin"></span></a></li>
+                        <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Header Upper -->
+    <div class="header-upper">
+        <div class="auto-container">
+            <div class="inner-container">
+                <!--Nav Box-->
+                <div class="nav-outer clearfix">
+                    <!--Logo-->
+                    <div class="logo-box">
+                        <div class="logo"><a href="index.html"><img src="assets/images/logo-2.png" alt=""></a></div>
+                    </div>
+                    <!--Mobile Navigation Toggler-->
+                    <div class="mobile-nav-toggler"><span class="icon fal fa-bars"></span></div>
+
+                    <!-- Main Menu -->
+                    <nav class="main-menu navbar-expand-md navbar-light">
+                        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                            <ul class="navigation clearfix">
+                                <li class="dropdown"><a href="index.html">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home page 01</a></li>
+                                        <li><a href="index-2.html">Home page 02</a></li>
+                                        <li><a href="index-3.html">Home page 03</a></li>
+                                        <li><a href="index-4.html">Home page 04</a></li>
+                                        <li><a href="onepage.html">Onepage Version</a></li>
+                                        <li><a href="rtl-home.html">Home RTL</a></li>
+                                        <li class="dropdown"><a href="index.html">Header Styles</a>
+                                            <ul>
+                                                <li><a href="index.html">Header Style One</a></li>
+                                                <li><a href="index-2.html">Header Style Two</a></li>
+                                                <li><a href="index-3.html">Header Style Three</a></li>
+                                                <li><a href="index-4.html">Header Style Four</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li class="dropdown"><a href="#">Services</a>
+                                    <ul>
+                                        <li><a href="services-1.html">Services One</a></li>
+                                        <li><a href="services-2.html">Services Two</a></li>
+                                        <li><a href="services-details.html">Services Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Portfolio</a>
+                                    <ul>
+                                        <li><a href="portfolio-1.html">Portfolio One</a></li>
+                                        <li><a href="portfolio-2.html">Portfolio Two</a></li>
+                                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">News</a>
+                                    <ul>
+                                        <li><a href="blog-grid.html">Blog Grid</a></li>
+                                        <li><a href="blog-standard.html">Blog Standard</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="careers.html">Careers</a></li>
+                                        <li><a href="team.html">Team</a></li>
+                                        <li><a href="team-details.html">Team Details</a></li>
+
+                                        <li class="dropdown"><a href="shop.html">Shop</a>
+                                            <ul>
+                                                <li><a href="shop.html">Products</a></li>
+                                                <li><a href="product-details.html">Product Details</a></li>
+                                                <li><a href="shopping-cart.html">Shopping Cart</a></li>
+                                                <li><a href="checkout.html">Checkout</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="faq.html">Faq's</a></li>
+                                        <li><a href="404.html">404</a></li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <!-- Main Menu End-->
+
+                    <!-- Link Btn-->
+                    <div class="link-btn"><a href="#" class="theme-btn btn-style-one"><span class="btn-title">Get A Quote</span></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Header Upper-->
+@show
