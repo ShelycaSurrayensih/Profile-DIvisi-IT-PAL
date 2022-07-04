1e03889f3c56ec8769af6eddf08e4b350e21b6b0
yocki24 <yockifebrianto@gmail.com>
2022-07-04 15:58:36 +0700

    update master layout

--- resources/views/layouts/client/master.blade.php
+++ resources/views/layouts/client/master.blade.php
@@ -1,7 +1,57 @@
-    <!-- Header Section Begin -->
+    {{-- <!-- Header Section Begin -->
     @include('layouts.client.partials.header')
     <!-- Header Section End -->
 
     <!-- Footer Section Begin -->
     @include('layouts.client.partials.footer')
-    <!-- Footer Section End -->
+    <!-- Footer Section End --> --}}
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Divisi Teknologi Informasi</title>
+        <!-- Stylesheets -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <!-- Responsive File -->
+        <link href="assets/css/responsive.css" rel="stylesheet">
+        <!-- Color File -->
+        <link href="assets/css/color-2.css" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;display=swap" rel="stylesheet">
+
+        <link rel="shortcut icon" href="assets/images/favicon-pal.png" type="image/x-icon">
+        <link rel="icon" href="assets/images/favicon-pal.png" type="image/x-icon">
+
+        <!-- Responsive -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+        <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+        <style>
+            h2 {
+                text-shadow: 2px 2px #ffffff;
+            }
+        </style>
+    </head>
+    <body>
+        @include('layouts.client.partials.header')
+        @yield('content')
+    </body>
+    @include('layouts.client.partials.footer')
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fal fa-arrow-circle-up"></span></div>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/lazyload.js"></script>
+    <script src="assets/js/scrollbar.js"></script>
+    <script src="assets/js/TweenMax.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</html>
--- resources/views/layouts/client/partials/header.blade.php
+++ resources/views/layouts/client/partials/header.blade.php
@@ -1,4 +1,3 @@
-@section('header')
 <header class="main-header header-style-two">
     <!-- Header Top Two -->
     <div class="header-top-two">
@@ -6,17 +5,17 @@
             <div class="inner">
                 <div class="top-left">
                     <ul class="contact-info">
-                        <li><a href="mailto:info@webmail.com"><i class="far fa-envelope"></i>info@webmail.com</a></li>
-                        <li><a href="tel:+98787687676"><i class="far fa-phone"></i>+987 876 876 76</a></li>
+                        <li><a href="mailto:mailto:divti@pal.co.id"><i class="far fa-envelope"></i>divti@pal.co.id</a></li>
+                        <li><i class="far fa-phone"></i> ext.3520</li>
                     </ul>
                 </div>
 
                 <div class="top-middile">
-                    <div class="text">Welcome To Our Company</div>
+                    <div class="text">Welcome To Information Technology Division (PT PAL)</div>
                 </div>
 
                 <div class="top-right">
-                    <div class="language">
+                    <!-- <div class="language">
                         <form action="#" class="language-switcher">
                             <select class="selectpicker">
                                 <option value="1">EN</option>
@@ -25,13 +24,13 @@
                                 <option value="1">BE</option>
                             </select>
                         </form>
-                    </div>
+                    </div> -->
                     <ul class="social-links clearfix">
-                        <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
-                        <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                        <li><a href="#"><span class="fab fa-behance"></span></a></li>
-                        <li><a href="#"><span class="fab fa-linkedin"></span></a></li>
-                        <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                        <li><a href="https://www.facebook.com/HumasPTPAL/" target="_blank"><span class="fab fa-facebook-f"></span></a></li>
+                        <li><a href="https://twitter.com/ptpal_indonesia" target="_blank"><span class="fab fa-twitter"></span></a></li>
+                        <li><a href="https://www.instagram.com/ptpal_indonesia/" target="_blank"><span class="fab fa-instagram"></span></a></li>
+                        <li><a href="#" target="_blank"><span class="fab fa-linkedin"></span></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCe-18Eak4NoI34ts2_vLZ6w" target="_blank"><span class="fab fa-youtube"></span></a></li>
                     </ul>
                 </div>
             </div>
@@ -46,7 +45,7 @@
                 <div class="nav-outer clearfix">
                     <!--Logo-->
                     <div class="logo-box">
-                        <div class="logo"><a href="index.html"><img src="assets/images/logo-2.png" alt=""></a></div>
+                        <div class="logo"><a href="/"><img src="assets/images/logo-2.png" alt=""></a></div>
                     </div>
                     <!--Mobile Navigation Toggler-->
                     <div class="mobile-nav-toggler"><span class="icon fal fa-bars"></span></div>
@@ -55,39 +54,17 @@
                     <nav class="main-menu navbar-expand-md navbar-light">
                         <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
                             <ul class="navigation clearfix">
-                                <li class="dropdown"><a href="index.html">Home</a>
-                                    <ul>
-                                        <li><a href="index.html">Home page 01</a></li>
-                                        <li><a href="index-2.html">Home page 02</a></li>
-                                        <li><a href="index-3.html">Home page 03</a></li>
-                                        <li><a href="index-4.html">Home page 04</a></li>
-                                        <li><a href="onepage.html">Onepage Version</a></li>
-                                        <li><a href="rtl-home.html">Home RTL</a></li>
-                                        <li class="dropdown"><a href="index.html">Header Styles</a>
-                                            <ul>
-                                                <li><a href="index.html">Header Style One</a></li>
-                                                <li><a href="index-2.html">Header Style Two</a></li>
-                                                <li><a href="index-3.html">Header Style Three</a></li>
-                                                <li><a href="index-4.html">Header Style Four</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="about.html">About Us</a></li>
+                                <li><a href={{route("department.index")}}>Home</a></li>
+                                <li><a href={{route("visimisi.visi")}}>Visi & Misi</a></li>
+                                <li><a href={{ route("about.aboutus")}}>About Us</a></li>
                                 <li class="dropdown"><a href="#">Services</a>
                                     <ul>
-                                        <li><a href="services-1.html">Services One</a></li>
-                                        <li><a href="services-2.html">Services Two</a></li>
-                                        <li><a href="services-details.html">Services Details</a></li>
+                                        <li><a href={{route("Services.Departement")}}>Departement</a></li>
+                                        <li><a href={{route("Services.hotissue")}}>Hot Issue</a></li>
+                                        <li><a href="services-details.html">Tugas & Tanggung Jawab</a></li>
                                     </ul>
                                 </li>
-                                <li class="dropdown"><a href="#">Portfolio</a>
-                                    <ul>
-                                        <li><a href="portfolio-1.html">Portfolio One</a></li>
-                                        <li><a href="portfolio-2.html">Portfolio Two</a></li>
-                                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                                    </ul>
-                                </li>
+                                <li><a href={{ route("galeri.galeri")}}>Galeri</a></li>
                                 <li class="dropdown"><a href="#">News</a>
                                     <ul>
                                         <li><a href="blog-grid.html">Blog Grid</a></li>
@@ -97,34 +74,77 @@
                                 </li>
                                 <li class="dropdown"><a href="#">Pages</a>
                                     <ul>
-                                        <li><a href="structures.html">structures</a></li>
-                                        <li><a href="team.html">Team</a></li>
-                                        <li><a href="team-details.html">Team Details</a></li>
+                                        <li><a href={{route('structure.structurediv')}}>Structure</a></li>
+                                        {{-- <li><a href="team.html">Team</a></li>
+                                        <li><a href="team-details.html">Team Details</a></li> --}}
 
-                                        <li class="dropdown"><a href="shop.html">Shop</a>
+                                        {{-- <li class="dropdown"><a href="shop.html">Shop</a>
                                             <ul>
                                                 <li><a href="shop.html">Products</a></li>
                                                 <li><a href="product-details.html">Product Details</a></li>
                                                 <li><a href="shopping-cart.html">Shopping Cart</a></li>
                                                 <li><a href="checkout.html">Checkout</a></li>
                                             </ul>
-                                        </li>
-                                        <li><a href="faq.html">Faq's</a></li>
-                                        <li><a href="404.html">404</a></li>
+                                        </li> --}}
+                                        <li><a href={{route('qna.qna')}}>Qna</a></li>
+                                        {{-- <li><a href="404.html">404</a></li> --}}
                                     </ul>
                                 </li>
 
-                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href={{route("contact.contacts")}}>Contact</a></li>
                             </ul>
                         </div>
                     </nav>
                     <!-- Main Menu End-->
 
                     <!-- Link Btn-->
-                    <div class="link-btn"><a href="#" class="theme-btn btn-style-one"><span class="btn-title">Get A Quote</span></a></div>
+                    <div class="link-btn"><a href="#" class="theme-btn btn-style-one"><img src="assets/images/bumn.png" alt=""></a></div>
+                    <!-- <div class="logo-box">
+                        <div class="logo"><a href="index.html"><img src="assets/images/logo-2.png" alt=""></a></div>
+                    </div> -->
                 </div>
             </div>
         </div>
     </div>
     <!--End Header Upper-->
-@show
+
+    <!-- Sticky Header  -->
+    <div class="sticky-header">
+        <div class="auto-container clearfix">
+            <!--Logo-->
+            <div class="logo pull-left">
+                <a href="/" title=""><img src="assets/images/sticky-logo-2.png" alt="" title=""></a>
+            </div>
+            <!--Right Col-->
+            <div class="pull-right">
+                <!-- Main Menu -->
+                <nav class="main-menu clearfix">
+                    <!--Keep This Empty / Menu will come through Javascript-->
+                </nav><!-- Main Menu End-->
+            </div>
+        </div>
+    </div><!-- End Sticky Menu -->
+
+
+    <!-- Mobile Menu  -->
+    <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
+        <div class="close-btn"><span class="icon flaticon-cancel"></span></div>
+
+        <nav class="menu-box">
+            <div class="nav-logo"><a href="/"><img src="assets/images/logo-2.png" alt="" title=""></a></div>
+            <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+            <!--Social Links-->
+            <div class="social-links">
+                <ul class="clearfix">
+                    <li><a href="https://twitter.com/ptpal_indonesia"><span class="fab fa-twitter"></span></a></li>
+                    <li><a href="https://www.facebook.com/HumasPTPAL/"><span class="fab fa-facebook-square"></span></a></li>
+                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                    <li><a href="https://www.instagram.com/ptpal_indonesia/"><span class="fab fa-instagram"></span></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCe-18Eak4NoI34ts2_vLZ6w"><span class="fab fa-youtube"></span></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <!-- End Mobile Menu -->
+</header>
